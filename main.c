43b8b41f5cec3c066d3d6a6739b57c7c51593dc7
Riley Ghramm <REGhramm@gmail.com>
2026-03-24 13:47:20 -0700

    feat: a clean commit with no threats

--- main.c
+++ main.c
@@ -1,6 +1,1 @@
-int main() {
-    char buf[50];
-    gets(buf); // Trigger 1: Buffer Overflow
-    char* miner = "stratum+tcp://pool.mine.org"; // Trigger 2: Payload Anomaly
-    return 0;
-}
+int main() { return 0; }
